bebdeb7ca9130112829b72bf96daf519415bc33f
Zhuyuanhui <1643515459@qq.com>
2023-09-23 20:44:50 +0800

    第一版

--- 云存储管理系统.cpp
+++ 云存储管理系统.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	cout << "服务器正在启动" << endl;
 	Ikernel* k = TCPkernel::gettcpkernel();
-	cout << "文件位置为d:/disk" << endl;
+	cout << "文件存储位置为d:/disk" << endl;
+
 	if (k->Opensqlandnet())//初始化网络服务，sql数据库服务
 	{
 		cout << "服务器已经成功启动" << endl;
